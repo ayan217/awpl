bdf725627500f3a336994add35c0ff1923c676de
Ayan <karmakarayan217@gmail.com>
2023-05-23 19:10:58 +0530

    product page style and slide will be fixed from here

--- assets/frontend/js/custom.js
+++ assets/frontend/js/custom.js
@@ -87,50 +87,50 @@ var incrementMinus = buttonMinus.click(function() {
 
 
 
-$('.slider-for').slick({
-  slidesToShow: 1,
-  // slidesToScroll: false,
-  swipe: false,
-  arrows: true,
-  fade: true,
-  asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  vertical:true,
-  asNavFor: '.slider-for',
-  dots: false,
-  focusOnSelect: true,
-  verticalSwiping:false,
-  nav: true,
-  responsive: [
-  {
-      breakpoint: 992,
-      settings: {
-        vertical: false,
-      }
-  },
-  {
-    breakpoint: 768,
-    settings: {
-      vertical: false,
-    }
-  },
-  {
-    breakpoint: 580,
-    settings: {
-      vertical: false,
-      slidesToShow: 3,
-    }
-  },
-  {
-    breakpoint: 380,
-    settings: {
-      vertical: false,
-      slidesToShow: 2,
-    }
-  }
-  ]
-});
+// $('.slider-for').slick({
+//   slidesToShow: 1,
+//   // slidesToScroll: false,
+//   swipe: false,
+//   arrows: true,
+//   fade: true,
+//   asNavFor: '.slider-nav'
+// });
+// $('.slider-nav').slick({
+//   slidesToShow: 4,
+//   slidesToScroll: 1,
+//   vertical:true,
+//   asNavFor: '.slider-for',
+//   dots: false,
+//   focusOnSelect: true,
+//   verticalSwiping:false,
+//   nav: true,
+//   responsive: [
+//   {
+//       breakpoint: 992,
+//       settings: {
+//         vertical: false,
+//       }
+//   },
+//   {
+//     breakpoint: 768,
+//     settings: {
+//       vertical: false,
+//     }
+//   },
+//   {
+//     breakpoint: 580,
+//     settings: {
+//       vertical: false,
+//       slidesToShow: 3,
+//     }
+//   },
+//   {
+//     breakpoint: 380,
+//     settings: {
+//       vertical: false,
+//       slidesToShow: 2,
+//     }
+//   }
+//   ]
+// });
 
